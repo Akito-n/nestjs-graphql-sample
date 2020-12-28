1a1d166a54b54d0fc4a761d919884db76cdf9562
Akito-n <a.nagai@mof-mof.co.jp>
2020-12-28 22:15:26 +0900

    argsの型

--- src/authors/models/dto/get-author.args.ts
+++ src/authors/models/dto/get-author.args.ts
@@ -1,4 +1,13 @@
+import { ArgsType, Field } from '@nestjs/graphql';
 import { MinLength } from 'class-validator' 
 
+@ArgsType()
+class GetAuthorArgs {
+  @Field({nullable: true})
+  firstName?: string
 
+  @Field({defaultValue: ''})
+  @MinLength(3)
+  lastName: string
+}
 
