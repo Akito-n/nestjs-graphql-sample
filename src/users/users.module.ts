51d3f462319999ed3de9e45dd2bbdb9067bf0544
Akito-n <a.nagai@mof-mof.co.jp>
2020-12-30 00:17:54 +0900

    serviceを追加

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module } from "@nestjs/common";
 import { User } from "./user.entity";
+import { UsersService } from "./users.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [],
+  providers: [UsersService],
+  exports: [TypeOrmModule]
 })
 
 export class UserModule {}
